3ff5cfeffc5a7ea375a47818f13dfa03e2628e75
Rhys <rhys.powell@tcs.com>
2020-04-03 17:12:35 +0100

    add additional task

--- db/db.js
+++ db/db.js
@@ -3,6 +3,11 @@ const todos =  [
       id: 1,
       title: "lunch",
       description: "Go for lunc by 2pm"
+    },
+    {
+      id: 2,
+      title: "Dinner",
+      description: "Go for dinner by 8pm"
     }
 ];
 
